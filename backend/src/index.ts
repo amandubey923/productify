941bbd133dc67932abdaff64263b45a2eed280f2
amandubey923 <kumaraman19137@gmail.com>
2025-12-30 23:23:28 +0530

    initial setup

---
+++ backend/src/index.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+
+import { ENV } from "./config/env";
+import { clerkMiddleware } from "@clerk/express";
+
+import userRoutes from "./routes/userRoutes";
+import productRoutes from "./routes/productRoutes";
+import commentRoutes from "./routes/commentRoutes";
+
+const app = express();
+
+app.use(cors({ origin: ENV.FRONTEND_URL, credentials: true }));
+
+app.use(clerkMiddleware()); 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+app.get("/api/health", (req, res) => {
+  res.json({
+    message: "Welcome to Productify API - Powered by PostgreSQL, Drizzle ORM & Clerk Auth",
+    endpoints: {
+      users: "/api/users",
+      products: "/api/products",
+      comments: "/api/comments",
+    },
+  });
+});
+
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/comments", commentRoutes);
+
+if (ENV.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+ 
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
+app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
